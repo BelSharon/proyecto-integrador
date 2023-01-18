178681d074fdbe214e6f17ce2cbcc43822e23cde
rogegar <rogegarhdz@gmail.com>
2023-01-17 23:55:25 -0600

    se cambió el mensaje de campos invalidos en la pagina de  contactanos y el regex de teléfono para que no admita solo ceros

--- js/contact.js
+++ js/contact.js
@@ -20,39 +20,58 @@ document.getElementById('form')
         mensaje.value = mensaje.value.trim();
         alertError.style.display = "none" //Quitar mensaje de error
         alertError.innerHTML = "";
+       
+        function mostrarError(mensaje1, campo, idError) {
+            const errorElement = document.getElementById(idError);
+            if (errorElement) {
+                errorElement.style.display = "block";
+                errorElement.innerHTML = mensaje1;
+                campo.style.border = "solid red 1px";
+            } else {
+                console.error(`No existe elemento con id: ${idError}`);
+            }
+        }
+       
+       
+       
+       
+       
+       
+       
         if (mensaje.value.trim().replaceAll("", "").length < 20) {
-            alertError.innerHTML = "El mensaje debe contener 20 caracteres o más"
-            alertError.style.display = "block";
+            mostrarError("El mensaje debe tener al menos 20 caracteres", mensaje, "error_mensaje_id");
             mensaje.focus();
             mensaje.select();
             mensaje.style.border = "solid red 1px" // color rojo al no estar correcto
         } else {
+            document.getElementById("error_mensaje_id").style.display = "none";
             mensaje.style.border = "solid green 1px" // color verde al  estar correcto
             validos++;
         }
         if (nombre.value.length<3) {
-            alertError.style.display="block";
-            alertError.innerHTML += "<br>El nombre no es válido.";
+            mostrarError("El nombre no es válido", nombre, "error_nombre_id");
             nombre.style.border="solid red 1px"
         }else{
+            document.getElementById("error_nombre_id").style.display = "none";
             nombre.style.border="solid green 1px"
             validos++;
         }
         if (correo.value.match(emailRegex)==null) {
-            alertError.style.display="block";
-            alertError.innerHTML += "<br>El correo electrónico no es válido.";
+            mostrarError("El correo electrónico no es válido.", correo, "error_email_id");
             correo.style.border="solid red 1px"
         }else{
+            document.getElementById("error_email_id").style.display = "none";
             correo.style.border="solid green 1px"
             validos++;
         }        
 
-        let numRegex = /[0-9]{10}/;
+        let numRegex = /^([1-9]{2})\d{8}$/;
         if (telefono.value.match(numRegex) == null) {
-            alertError.style.display = "block";
-            alertError.innerHTML += "<br>El teléfono no es válido.";
+            
+            mostrarError("El teléfono no es válido.", telefono, "error_telefono_id");
             telefono.style.border = "solid red 1px"
         } else {
+            document.getElementById("error_telefono_id").style.display = "none";
             telefono.style.border = "solid green 1px"
             validos++;
         }
